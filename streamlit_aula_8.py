d4cb900fc42924b548957dcb0f87215a94a96392
pbitgomes <88545579+pbitgomes@users.noreply.github.com>
2024-11-13 11:18:00 -0300

    questao_3

--- streamlit_aula_8.py
+++ streamlit_aula_8.py
@@ -4,6 +4,7 @@ st.title('Isso é um título')
 
 st.write('Vamos aprender streamlit juntos')
 
+# questão 2
 st.title("Este é o título do app")
 st.header("Este é o subtítulo")
 st.subheader("Este é o terceiro subtítulo")
@@ -11,3 +12,7 @@ st.markdown("Este é texto")
 st.caption("Esta é a a legenda")
 st.code("x=2021")
 st.latex(r''' a+a r^1+a r^2+a r^3 ''')
+
+# questão 3
+resposta = st.select_slider('Escolha um valor de 0 a 100', 0, 100)
+st.write(resposta)
