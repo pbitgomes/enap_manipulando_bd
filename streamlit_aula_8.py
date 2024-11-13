bcba0806109fbddb787b6c642137f957b35c57e1
pbitgomes <88545579+pbitgomes@users.noreply.github.com>
2024-11-13 11:19:52 -0300

    lista_de_tipos

--- streamlit_aula_8.py
+++ streamlit_aula_8.py
@@ -16,3 +16,33 @@ st.latex(r''' a+a r^1+a r^2+a r^3 ''')
 # questão 3
 resposta = st.slider('Escolha um valor de 0 a 100', 0, 100)
 st.write(resposta)
+
+
+st.title('Tipos de campos possíveis')
+
+# criando elementos gráficos
+'''Informar como colher os dados através de variáveis'''
+x = st.checkbox('Sim')
+st.write(x)
+
+# fazer o mesmo com alguns dos comandos abaixo
+st.title(x)
+st.button('Clique')
+st.radio('Selecione seu gênero',['Masculino','Feminino'])
+st.selectbox('Selecione seu gênero',['Masculino','Feminino'])
+st.multiselect('Escolha um departamento',['DCS', 'DE', 'DIR'])
+st.select_slider('Selecione uma resposta', ['Ruim', 'Bom', 'Excelente'])
+st.slider('Selecione um número', 0,50)
+st.number_input('Selecione um número', 0,10)
+st.text_input('Endereço de e-mail')
+st.date_input('Data de viagem')
+st.time_input('Tempo de escola')
+st.text_area('Descrição')
+st.file_uploader('Atualize uma foto')
+st.color_picker('Escolha sua cor favorita')
+
+# mensagens de status
+st.success("Você conseguiu!")
+st.error("Erro!")
+st.warning("Advertência")
+st.info("Esta é uma informação")
