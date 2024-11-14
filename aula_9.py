58bf868c8a30370d787f99ef7e723f43fe122725
pbitgomes <88545579+pbitgomes@users.noreply.github.com>
2024-11-14 09:44:16 -0300

    aula_9_questao_3.py

--- aula_9.py
+++ aula_9.py
@@ -4,6 +4,7 @@ import pandas as pd
 st.title('Localização das comunidades quilombolas (2022)')
 df = pd.read_csv('https://raw.githubusercontent.com/adrianalite/datasets/main/BR_LQs_CD2022.csv')
 
+# questão 2
 df.fillna(0, inplace=True)
 df.drop(columns=['Unnamed: 0'], inplace=True)
 list = ['Lat_d', 'Long_d']
@@ -17,3 +18,10 @@ dadosFiltrados = df[df['NM_UF'] == estadoFiltro]
 if st.checkbox('Mostrar tabela'):
   st.write(dadosFiltrados)
 st.map(dadosFiltrados, latitude="Lat_d", longitude="Long_d")
+
+# questão 3
+qtdeMunicipios = len(df['NM_MUNIC'].unique())
+st.write("A quantidade de municípios com localização quilombola é " + str(qtdeMunicipios))
+
+qtdeComunidades = len(df['NM_AGLOM'].unique())
+st.write("A quantidade de comunidades quilombolas é " + str(qtdeComunidades))
